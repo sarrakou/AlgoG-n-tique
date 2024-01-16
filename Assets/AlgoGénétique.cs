0399decc16e1fbe50e0dc85354e848a79e505c91
Elodie0207 <elodieguldas@gmail.com>
2024-01-16 21:09:35 +0100

    ConversionBitFloat

--- Assets/AlgoGénétique.cs
+++ Assets/AlgoGénétique.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -22,14 +23,45 @@ public class AlgoGénétique : MonoBehaviour
     [SerializeField]private GameObject BonHommePrefab;
     
     string outputString = "";
-
-    string ConvertirEnBits(Vector3 inputVector)
+    
+    int FloatToInt(float floatValue)
     {
-        outputString += BitConverter.ToString(BitConverter.GetBytes(inputVector.x));
-        outputString += BitConverter.ToString(BitConverter.GetBytes(inputVector.y));
-        outputString += BitConverter.ToString(BitConverter.GetBytes(inputVector.z));
-      return outputString;  
+        return BitConverter.ToInt32(BitConverter.GetBytes(floatValue), 0);
     }
+
+    float IntToFloat(int intValue)
+    {
+        return BitConverter.ToSingle(BitConverter.GetBytes(intValue), 0);
+    }
+
+    
+    string ConvertToBit(Vector3 inputVector)
+    {
+        int xInt = FloatToInt(inputVector.x);
+        int yInt = FloatToInt(inputVector.y);
+        int zInt = FloatToInt(inputVector.z);
+
+        outputString = Convert.ToString(xInt, 2).PadLeft(32, '0')
+                       + Convert.ToString(yInt, 2).PadLeft(32, '0')
+                       + Convert.ToString(zInt, 2).PadLeft(32, '0');
+
+        return outputString;
+    }
+
+  
+    Vector3 ConvertToFloat(string binary)
+    {
+        string xBin = binary.Substring(0, 32);
+        string yBin = binary.Substring(32, 32);
+        string zBin = binary.Substring(64, 32);
+
+        float xFloat = IntToFloat(Convert.ToInt32(xBin, 2));
+        float yFloat = IntToFloat(Convert.ToInt32(yBin, 2));
+        float zFloat = IntToFloat(Convert.ToInt32(zBin, 2));
+
+        return new Vector3(xFloat, yFloat, zFloat);
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -100,8 +132,11 @@ public class AlgoGénétique : MonoBehaviour
 
             float parentY = Mathf.Max(joueur1.y, joueur2.y, joueur3.y );
         }
-        
-        print(ConvertirEnBits(new Vector3(9,10,3)));
+        /*Vector3 originalVector = new Vector3(9, 10, 3);
+        print("Original Vector3: " + originalVector);
+        print("Binary: " + ConvertToBit(originalVector));
+        Vector3 newVector = ConvertToFloat(outputString);
+        print("New Vector3: " + reconstructedVector);*/
         return parents;
     }
 }
