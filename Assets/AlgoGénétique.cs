f36059e310292617ed1c84a8f6f9cd06f74a06e5
Sarra Koussaier <95942823+sarrakou@users.noreply.github.com>
2024-01-17 16:19:34 +0100

    trying the main function

--- Assets/AlgoGénétique.cs
+++ Assets/AlgoGénétique.cs
@@ -16,44 +16,76 @@ public class AlgoGénétique : MonoBehaviour
 {
     private int NumGenerations=0;
     public int NumPoints=10;
-    private Vector3[] points;
+    private List<Vector3> points = new List<Vector3>();
     private BitArray[] ADN;
     private Vector3[] parents = new Vector3[2];
     private string[] children = new string[2];
     [SerializeField]private GameObject BonHommePrefab;
     
     string outputString = "";
-
-    string ConvertirEnBits(Vector3 inputVector)
+    
+    int FloatToInt(float floatValue)
     {
-        outputString += BitConverter.ToString(BitConverter.GetBytes(inputVector.x));
-        outputString += BitConverter.ToString(BitConverter.GetBytes(inputVector.y));
-        outputString += BitConverter.ToString(BitConverter.GetBytes(inputVector.z));
-      return outputString;  
+        return BitConverter.ToInt32(BitConverter.GetBytes(floatValue), 0);
+    }
+
+    float IntToFloat(int intValue)
+    {
+        return BitConverter.ToSingle(BitConverter.GetBytes(intValue), 0);
+    }
+
+    
+    string ConvertToBit(Vector3 inputVector)
+    {
+        int xInt = FloatToInt(inputVector.x);
+        int yInt = FloatToInt(inputVector.y);
+        int zInt = FloatToInt(inputVector.z);
+
+        outputString = Convert.ToString(xInt, 2).PadLeft(32, '0')
+                       + Convert.ToString(yInt, 2).PadLeft(32, '0')
+                       + Convert.ToString(zInt, 2).PadLeft(32, '0');
+
+        return outputString;
+    }
+
+  
+    Vector3 ConvertToFloat(string binary)
+    {
+        string xBin = binary.Substring(0, 32);
+        string yBin = binary.Substring(32, 32);
+        string zBin = binary.Substring(64, 32);
+
+        float xFloat = IntToFloat(Convert.ToInt32(xBin, 2));
+        float yFloat = IntToFloat(Convert.ToInt32(yBin, 2));
+        float zFloat = IntToFloat(Convert.ToInt32(zBin, 2));
+
+        return new Vector3(xFloat, yFloat, zFloat);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        points = new Vector3[NumPoints];
-        InitialisationPopulation();
-        CalculFitness();
-        SelectionnerParents();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       /*  while()
+        while(NumGenerations<10)
         {
             NumGenerations++;
-            
-            Recombinaison();
-            Mutation();
-        } */
+            InitialisationPopulation();
+            CalculFitness();
+            print(NumPoints);
+            parents = SelectionnerParents();
+            children = CrossOver(ConvertToBit(parents[0]), ConvertToBit(parents[1]));
+            Mutation(children[0]);
+            points.Add(ConvertToFloat(children[0]));
+            Mutation(children[1]);
+            points.Add(ConvertToFloat(children[1]));
+            supppLesDeuxPlusNuls();
+            print(NumPoints);
+            NumPoints +=2;
+            print(NumPoints);
+        } 
     }
 
-/*     string[] CrossOver(string parent1, string parent2)
+
+    string[] CrossOver(string parent1, string parent2)
     {
         int randomPlacement = Random.Range(0, parent1.Length);
         string child1 = parent1.Substring(0, randomPlacement) + parent2.Substring(randomPlacement, parent2.Length-randomPlacement);
@@ -61,9 +93,10 @@ public class AlgoGénétique : MonoBehaviour
         
         children[0] = child1;
         children[1] = child2;
+        
 
         return children;
-    } */
+    } 
 
     string Mutation(string ADN)
     {
@@ -110,8 +143,9 @@ public class AlgoGénétique : MonoBehaviour
         Vector3 joueur1 = new Vector3(0,0,0);
         Vector3 joueur2 = new Vector3(0,0,0);
         Vector3 joueur3 = new Vector3(0,0,0);
+        Vector3[] p = new Vector3[2];
 
-        for (int i=0 ; i<2; i++)
+        for (int i=0; i<2; i++)
         {
             joueur1 = points[Random.Range(0, NumPoints)];
             joueur2 = points[Random.Range(0, NumPoints)];
@@ -126,10 +160,54 @@ public class AlgoGénétique : MonoBehaviour
                 joueur3 = points[Random.Range(0, NumPoints)];
             }
 
-            float parentY = Mathf.Max(joueur1.y, joueur2.y, joueur3.y );
+            float parentY = Mathf.Max(joueur1.y, joueur2.y, joueur3.y);
+            if (parentY==joueur1.y)
+            {
+                p[i] = joueur1;
+            } else if (parentY==joueur2.y)
+            {
+                p[i] = joueur2;
+            } else
+            {
+                p[i] = joueur3;
+            }
+            
         }
-        
-        print(ConvertirEnBits(new Vector3(9,10,3)));
-        return parents;
+        return p;
     }
+
+    //selection de la plus nul 
+    int SelectionnerLePlusNul()
+    {
+        Vector3 le_plus_bas = points[0];
+        int id = 0;
+
+        for (int i = 1; i < points.Count; i++)
+        {
+            if (points[i].y < le_plus_bas.y)
+            {
+                le_plus_bas = points[i];
+                id = i;
+            }
+        }
+        return id;
+
+    }
+     //methode pour supprimer les 2 plus nuls
+    void supppLesDeuxPlusNuls()
+    {
+        points.RemoveAt(SelectionnerLePlusNul());
+        points.RemoveAt(SelectionnerLePlusNul());
+        NumPoints -= 2;
+    }
+
+    //methode pour suprimer un element d'un tableau 
+  /*   static void RemoveAt<T>(ref T[] arr, int index)
+    {
+        for (int a = index; a < arr.Length - 1; a++)
+        {
+            arr[a] = arr[a + 1];
+        }
+        Array.Resize(ref arr, arr.Length - 1);
+    } */
 }
